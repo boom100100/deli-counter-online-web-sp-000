dbd792dd7b337f6c326229c0d8003bd2af0da0cc
Bernadette Davis <bsd231@nyu.edu>
2019-05-16 20:55:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+
+def line(input)
+  if input.size == 0
+    puts "The line is currently empty."
+  else
+    string = "The line is currently:"
+
+    input.each_with_index do |person, index|
+      string = string + " #{index + 1}. #{person}"
+    end
+
+    puts string
+  end
+end
+
+def take_a_number(deli, person)
+  if deli.size == 0
+  else
+  end
+
+    deli.push(person)
+    deli.each_with_index { |name, index|
+      puts "Welcome, #{name}. You are number #{index + 1} in line."
+    }
+end
+
+def now_serving(deli)
+  if deli.size == 0
+    "There is nobody waiting to be served!"
+  else
+    deli.pop
+  end
+end
